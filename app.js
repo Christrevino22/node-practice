c22b26a26b8396b6fc6803894ee84c65df252627
Christopher Trevino <chrisdadef@gmail.com>
2021-03-31 20:56:58 -0500

    safety

--- app.js
+++ app.js
@@ -7,5 +7,11 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  res.send('THIS IS COMING FROM EXPRESS YOU TRICK!!!')
+  // res.send('THIS IS COMING FROM EXPRESS YOU TRICK!!!')
+  res.sendFile('./views/inde.html', {root:__dirname })
+})
+
+app.get('/about', (req, res) => {
+  // res.send('THIS IS MY ABOUT PAGE TRICK!!!') 
+  res.sendFile('./views/about.html', {root: __dirname})
 })
